02aed08ffcd3bac94d3b8da1cb1f7fa390330aa1
mareklode <mareklode@gmx.de>
2019-11-04 11:45:44 +0300

    added structure for reusable images

--- scripts/mel/mel_patina.js
+++ scripts/mel/mel_patina.js
@@ -4,20 +4,31 @@ define(['canvas', 'createPattern', 'filter'], function( canvas, createPattern, f
     // http://codeblog.cz/vanilla/inside.html#set-element-html
 
     function patina (domElement, parameters) {
-        var self = this;
+        var self = this,
+            createPatina;
+        
         self.domElement = domElement;
         self._parameters = self._completeParameters( parameters, self.domElement );
         
 
         console.log('###### - Patina - ######');
-        console.log(self);
-        
+        // console.log(self);
         self.myCanvas = canvas.newCanvas( self._parameters.width, self._parameters.height );
-        var createPatina = self._evaluateLayerNode( 
-                self._parameters.patina, 
-                self._parameters.width, 
+
+        self.reusableImages = {};
+        this._parameters.reusableImages.forEach( function (value) {
+            self.reusableImages[value.id] = self._evaluateLayerNode(
+                value,
+                self._parameters.width,
                 self._parameters.height
             );
+        });
+       
+        createPatina = self._evaluateLayerNode( 
+            self._parameters.patina, 
+            self._parameters.width, 
+            self._parameters.height
+        );
 
         if (createPatina.grey) {
             // copy img byte-per-byte into our ImageData
@@ -37,6 +48,7 @@ define(['canvas', 'createPattern', 'filter'], function( canvas, createPattern, f
             }
         }
 
+        console.log(this);
 
         self.myCanvas.context.putImageData( self.myCanvas.img, 0, 0 );
         self._paintCanvas( self.myCanvas, self.domElement );
@@ -71,7 +83,6 @@ define(['canvas', 'createPattern', 'filter'], function( canvas, createPattern, f
             var resultingImage = {};
 
             if (bottomLayer.grey && topLayer.grey) {
-                console.log(bottomLayer)
                 resultingImage.grey = bottomLayer.grey.map(function (value, index) {
                     return (value + topLayer.grey[index]) / 2;
                 });
@@ -89,12 +100,10 @@ define(['canvas', 'createPattern', 'filter'], function( canvas, createPattern, f
         _evaluateLayerNode: function ( layer, width, height ) {
             var resultingImage = false;
 
-            ///console.log("typeof layer: ",typeof layer, layer);
-
             if (typeof layer === "number") {
-                return Array.from( {length: width * height}, () => layer/256 );
+                var color = layer / 256;
+                return Array.from( {length: width * height}, () => color );
             }
-
             if (layer.type === "colorChannels") {
                 resultingImage = {};
                 resultingImage.red = this._evaluateLayerNode(layer.red, width, height);
@@ -102,33 +111,32 @@ define(['canvas', 'createPattern', 'filter'], function( canvas, createPattern, f
                 resultingImage.blue = this._evaluateLayerNode(layer.blue, width, height);
                 resultingImage.alpha = this._evaluateLayerNode(layer.alpha, width, height).grey;
             }
-
-            ///debugger
-
             if (layer.type === "combine") {
                 resultingImage = this._combine( 
                     this._evaluateLayerNode(layer.bottomLayer, width, height),
                     this._evaluateLayerNode(layer.topLayer, width, height)
                 );
-                console.log("evaluateLayerNode combine", resultingImage);
             }
             if (layer.type === "createPattern") {
                 resultingImage = {};
                 resultingImage = new createPattern( layer, width, height );
-                console.log("evaluateLayerNode createPattern", resultingImage);
+            }
+            if (layer.type === "reuseImage") {
+                console.log("reuse Image: ", layer.name, this.reusableImages[layer.name]);
+                resultingImage = this.reusableImages[layer.name];
             }
             if (resultingImage) {
                 if (layer.filter) {
                     layer.filter.forEach(element => {
                         // todo: rauskriegen ob das hier sequenziell abgearbeitet wird oder race conditions stören
-                        console.log("resultingImage: ", resultingImage);
                         resultingImage = new filter(resultingImage, element, width, height);
                     });
                 }
+                // if (layer.reuseName)
                 return resultingImage;
             } else {
                 console.log('layer type not recognized ',layer);
-                return null; // transparentes Array rückgeben oder woanders gracefully failen
+                return null; // Todo: transparentes Array rückgeben oder woanders gracefully failen
             }
         }, // _evaluateLayerNode()
 
