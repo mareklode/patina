230b275b2bb14f15d404326c79687a8511cb7962
mareklode <mareklode@gmx.de>
2025-09-28 12:17:42 +0200

    Playground for all createPattern pages

---
+++ scripts/modules/mel_playground.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function playground (domElement, parameters) {
+    const trigger = document.getElementById('js_patina_CallToAction');
+    const textarea = document.getElementById('js_patina_textarea');
+    const target = document.getElementById('js_patina_fromTextarea');
+
+    const calculatePatinaFromTextarea = function () {
+        // ich importiere relativ zum base href
+        import('/patina/scripts/modules/mel_patina.js')
+            .then((patina) => {
+                new patina.default(target, textarea.value);
+            });
+    };
+
+    trigger.addEventListener('click', calculatePatinaFromTextarea);
+    calculatePatinaFromTextarea();
+}
+
+export default playground;
